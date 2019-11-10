26496c0b149d6624dc301b0d19276b4af91deeef
Jay Chen <gcobs135102@gmail.com>
2019-11-10 10:57:39 +0800

    chore: add some gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,42 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Jay's Blog`,
+    description: `Blog - Jay`,
+    author: `Jay`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
+        name: 'content',
+        path: `${__dirname}/src/content`
       }
     },
+    `gatsby-plugin-nprogress`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: 'gatsby-plugin-layout',
+      options: {
+        component: require.resolve('./src/components/layout/index.tsx')
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jay's Blog`,
+        short_name: `Jay`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        display: `standalone`
+        //  icon: `src/images/gatsby-icon.png`
       }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-offline`
   ]
 };
